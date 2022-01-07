a20ceb2815fde33b66e533b701df67e36bbd67ea
Ashwin Baranwal <ashwinbaranwal2001@gmail.com>
2022-01-07 13:31:26 +0530

    Added server.js in backend; created new package.json and new node_modules for backend; thus separating backend completely from frontend for deployment

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -1,5 +1,8 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost/' + process.env.MONGODB_DB);
+// mongoose.connect('mongodb://localhost/' + process.env.MONGODB_DB);
+mongoose.connect(`mongodb+srv://${process.env.MONGO_ATLAS_USERNAME}:${process.env.MONGO_ATLAS_PASSWORD}@cluster0.slyww.mongodb.net/${process.env.MONGO_ATLAS_DB}?retryWrites=true&w=majority`)
+.then(()=> console.log('MongoDB connected....'))
+.catch((err) => console.log(err));
 
 const db=mongoose.connection;
 
